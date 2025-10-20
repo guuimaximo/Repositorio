3e9702f6aa7a65ee7e89dd17eafd37d85363b203
guuimaximo <guimaximo1321@gmail.com>
2025-10-20 19:50:00 -0300

    Create index.css

---
+++ src/index.css
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js'
+
+// PUXA DAS VARIÁVEIS DE AMBIENTE (Render)
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+export const supabase = createClient(supabaseUrl, supabaseAnonKey)
