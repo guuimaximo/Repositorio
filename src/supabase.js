8254114eb3037dbe8345ac67bde6dd620d1fe288
guuimaximo <guimaximo1321@gmail.com>
2025-10-21 09:03:12 -0300

    Update supabase.js

--- src/supabase.js
+++ src/supabase.js
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-// PUXA DAS VARIÁVEIS DE AMBIENTE (Render)
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
+// === CONFIGURAÇÃO INOVEQUATAI ===
+const supabaseUrl = "https://wboelthngddvkgvrwkbuu.supabase.co"
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indib2VsdGhuZ2RkdmtncnZ3a2J1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA5ODQxMzcsImV4cCI6MjA3NjU2MDEzN30.A3ylU8Tkx20VOD3EjOr3K7ir0J_jZrCfBNlzAOtODXg"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
