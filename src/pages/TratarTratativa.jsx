8e81b1ef19b225f36ac3ac7d84a781cb8e6eec61
guuimaximo <guimaximo1321@gmail.com>
2025-10-23 18:59:57 -0300

    Update TratarTratativa.jsx

--- src/pages/TratarTratativa.jsx
+++ src/pages/TratarTratativa.jsx
@@ -2,7 +2,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { supabase } from "../supabase";
-import Navbar from "../components/Navbar";
 
 export default function TratarTratativa() {
   const { id } = useParams();
