79a7f96eb249110dcccc8d6b58a3bda27566a3de
guuimaximo <guimaximo1321@gmail.com>
2025-10-24 16:15:10 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,35 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import { Routes, Route, Navigate } from "react-router-dom";
+import Layout from "./components/Layout";
 
 // Páginas
-import Dashboard from './pages/Dashboard';
-import CentralTratativas from './pages/CentralTratativas';
-import TratarTratativa from './pages/TratarTratativa';
-import ConsultarTratativa from './pages/ConsultarTratativa';
-import SolicitacaoTratativa from './pages/SolicitacaoTratativa';
-import Login from './pages/Login';
-import Avarias from './pages/Avarias';
-import CobrancasAvarias from './pages/CobrancasAvarias';
+import Dashboard from "./pages/Dashboard";
+import CentralTratativas from "./pages/CentralTratativas";
+import TratarTratativa from "./pages/TratarTratativa";
+import ConsultarTratativa from "./pages/ConsultarTratativa";
+import SolicitacaoTratativa from "./pages/SolicitacaoTratativa";
+import Avarias from "./pages/Avarias";
+import CobrancasAvarias from "./pages/CobrancasAvarias";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
-      <Navbar />
-      <Routes>
-        {/* Página inicial (Painel principal) */}
+    <Routes>
+      {/* Rotas protegidas com Sidebar e Navbar */}
+      <Route element={<Layout />}>
         <Route path="/" element={<Dashboard />} />
-
-        {/* Módulo de Tratativas */}
         <Route path="/central" element={<CentralTratativas />} />
         <Route path="/tratar/:id" element={<TratarTratativa />} />
         <Route path="/consultar/:id" element={<ConsultarTratativa />} />
         <Route path="/solicitar" element={<SolicitacaoTratativa />} />
-
-        {/* Módulo de Avarias */}
         <Route path="/avarias" element={<Avarias />} />
-        
-        {/* Módulo de Cobranças */}
         <Route path="/cobrancas" element={<CobrancasAvarias />} />
+      </Route>
 
-        {/* Login */}
-        <Route path="/login" element={<Login />} />
+      {/* Login sem layout */}
+      <Route path="/login" element={<Login />} />
 
-        {/* Rota padrão */}
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </div>
+      {/* Rota padrão */}
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
   );
 }
