0a4b349dbae1eb7cc499334e897172e853bc5fe3
guuimaximo <guimaximo1321@gmail.com>
2025-10-24 13:48:52 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,38 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
-import Navbar from './components/Navbar'
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-// Páginas
-import Dashboard from './pages/Dashboard'
-import CentralTratativas from './pages/CentralTratativas'
-import TratarTratativa from './pages/TratarTratativa'
-import ConsultarTratativa from './pages/ConsultarTratativa'
-import SolicitacaoTratativa from './pages/SolicitacaoTratativa'
-import Login from './pages/Login'
+import Home from "./pages/Home";
+import Dashboard from "./pages/Dashboard";
+import CentralTratativas from "./pages/CentralTratativas";
+import ConsultarTratativa from "./pages/ConsultarTratativa";
+import TratarTratativa from "./pages/TratarTratativa";
+import SolicitarTratativa from "./pages/SolicitacaoTratativa";
+import Login from "./pages/Login";
+import Cadastro from "./pages/Cadastro";
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
-      <Navbar />
+    <Router>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/central" element={<CentralTratativas />} />
-        <Route path="/tratar/:id" element={<TratarTratativa />} />
-        <Route path="/consultar/:id" element={<ConsultarTratativa />} />
-        <Route path="/solicitar" element={<SolicitacaoTratativa />} />
+        {/* PÁGINA INICIAL */}
+        <Route path="/" element={<Home />} />
+
+        {/* MÓDULO DE TRATATIVAS */}
+        <Route path="/central-tratativas" element={<CentralTratativas />} />
+        <Route path="/consultar-tratativa/:id" element={<ConsultarTratativa />} />
+        <Route path="/tratar-tratativa/:id" element={<TratarTratativa />} />
+        <Route path="/solicitar-tratativa" element={<SolicitarTratativa />} />
+
+        {/* MÓDULO DE COBRANÇA DE AVARIAS (em breve) */}
+        <Route path="/avarias" element={<h1 className='p-10'>🚧 Módulo de Avarias em construção</h1>} />
+
+        {/* DASHBOARD */}
+        <Route path="/dashboard" element={<Dashboard />} />
+
+        {/* LOGIN / CADASTRO */}
         <Route path="/login" element={<Login />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="/cadastro" element={<Cadastro />} />
       </Routes>
-    </div>
-  )
+    </Router>
+  );
 }
