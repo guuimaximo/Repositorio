3721fc7b40ea61dd1446419c6826789b15f9d8a4
guuimaximo <guimaximo1321@gmail.com>
2025-10-24 16:24:29 -0300

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,20 +1,21 @@
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 export default function Layout() {
-  const location = useLocation();
-  const hideNavbarPaths = ["/login"]; // onde o Navbar e Sidebar não aparecem
-
-  const hideNavbar = hideNavbarPaths.includes(location.pathname);
-
   return (
     <div className="flex min-h-screen bg-gray-50">
-      {!hideNavbar && <Sidebar />}
+      
+      {/* Sempre renderiza o Sidebar, pois este Layout só é usado em rotas protegidas */}
+      <Sidebar /> 
+      
       <div className="flex-1 flex flex-col">
-        {!hideNavbar && <Navbar />}
+        
+        {/* Sempre renderiza a Navbar */}
+        <Navbar />
+
         <main className="flex-1 p-4">
-          <Outlet />
+          <Outlet /> {/* Onde as páginas (Dashboard, Avarias, etc.) aparecem */}
         </main>
       </div>
     </div>
