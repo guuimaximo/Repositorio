5ab040259de7a2f0f9d34e30e9a05598a2a11541
guuimaximo <guimaximo1321@gmail.com>
2025-10-26 14:08:48 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,28 +8,31 @@ import TratarTratativa from "./pages/TratarTratativa";
 import ConsultarTratativa from "./pages/ConsultarTratativa";
 import SolicitacaoTratativa from "./pages/SolicitacaoTratativa";
 import Login from "./pages/Login";
-import Avarias from "./pages/Avarias";
+import LancarAvaria from "./pages/LancarAvaria"; // Nova página de Lançamento
 import CobrancasAvarias from "./pages/CobrancasAvarias";
+// A página 'Avarias.jsx' antiga não é mais usada
 
 export default function App() {
-  return (
-    <Routes>
-      {/* Rotas internas com Sidebar + Navbar */}
-      <Route element={<Layout />}>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/central" element={<CentralTratativas />} />
-        <Route path="/tratar/:id" element={<TratarTratativa />} />
-        <Route path="/consultar/:id" element={<ConsultarTratativa />} />
-        <Route path="/solicitar" element={<SolicitacaoTratativa />} />
-        <Route path="/avarias" element={<Avarias />} />
-        <Route path="/cobrancas" element={<CobrancasAvarias />} />
-      </Route>
+  return (
+    <Routes>
+      {/* Rotas internas com Sidebar + Navbar */}
+      <Route element={<Layout />}>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/central" element={<CentralTratativas />} />
+        <Route path="/tratar/:id" element={<TratarTratativa />} />
+        <Route path="/consultar/:id" element={<ConsultarTratativa />} />
+        <Route path="/solicitar" element={<SolicitacaoTratativa />} />
+        
+        {/* Rotas de Avarias Atualizadas */}
+        <Route path="/lancar-avaria" element={<LancarAvaria />} />
+        <Route path="/cobrancas" element={<CobrancasAvarias />} />
+      </Route>
 
-      {/* Página de Login sem layout */}
-      <Route path="/login" element={<Login />} />
+      {/* Página de Login sem layout */}
+      <Route path="/login" element={<Login />} />
 
-      {/* Redirecionamento padrão */}
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
-  );
+      {/* Redirecionamento padrão */}
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  );
 }
