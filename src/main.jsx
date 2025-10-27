6481c5eddc62c7cf4c4bdbc476bf3cdb526db82b
guuimaximo <guimaximo1321@gmail.com>
2025-10-26 22:39:52 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,16 @@
+// src/main.jsx
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
+import { AuthProvider } from './AuthContext' // 1. Importar
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <BrowserRouter>
+    <AuthProvider> {/* 2. Envolver o App */}
+      <App />
+    </AuthProvider>
+  </BrowserRouter>
 )
