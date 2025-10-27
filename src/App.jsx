48463920ad0129a3fa15a4db99a7aa94668fc4c2
guuimaximo <guimaximo1321@gmail.com>
2025-10-26 23:13:14 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import TratarTratativa from "./pages/TratarTratativa";
 import ConsultarTratativa from "./pages/ConsultarTratativa";
 import SolicitacaoTratativa from "./pages/SolicitacaoTratativa";
 import Login from "./pages/Login";
-import Cadastro from "./pages/Cadastro"; // 1. Importar Cadastro
+import Cadastro from "./pages/Cadastro"; // Importar Cadastro
 import LancarAvaria from "./pages/LancarAvaria"; 
 import CobrancasAvarias from "./pages/CobrancasAvarias";
 import AprovacaoAvarias from "./pages/AprovacaoAvarias"; 
@@ -18,7 +18,7 @@ import AprovacaoAvarias from "./pages/AprovacaoAvarias";
 export default function App() {
   return (
     <Routes>
-      {/* Rotas internas (protegidas) */}
+      {/* Rotas internas (protegidas pelo Layout) */}
       <Route element={<Layout />}>
         <Route path="/" element={<Dashboard />} />
         <Route path="/central" element={<CentralTratativas />} />
@@ -28,14 +28,16 @@ export default function App() {
         <Route path="/lancar-avaria" element={<LancarAvaria />} />
         <Route path="/aprovar-avarias" element={<AprovacaoAvarias />} />
         <Route path="/cobrancas" element={<CobrancasAvarias />} />
+        {/* Adicione outras rotas protegidas aqui */}
       </Route>
 
       {/* Rotas públicas (sem layout) */}
       <Route path="/login" element={<Login />} />
-      <Route path="/cadastro" element={<Cadastro />} /> {/* 2. Adicionar Rota */}
+      <Route path="/cadastro" element={<Cadastro />} /> {/* Rota de Cadastro */}
 
-      {/* Redirecionamento padrão (vamos ajustar isso no Layout) */}
-      <Route path="*" element={<Navigate to="/" replace />} />
+      {/* Redirecionamento para rotas não encontradas */}
+      {/* O Layout agora cuida do redirecionamento de não logados */}
+      <Route path="*" element={<Navigate to="/" replace />} /> 
     </Routes>
   );
 }
