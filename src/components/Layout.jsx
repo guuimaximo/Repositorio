d12ad5c645c528a0b49a7809d22e7245acd9f98e
guuimaximo <guimaximo1321@gmail.com>
2025-10-26 21:02:42 -0300

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,20 +1,20 @@
 import { Outlet, useLocation } from "react-router-dom";
-import Navbar from "./Navbar";
+// import Navbar from "./Navbar"; // Removido
 import Sidebar from "./Sidebar";
 
 export default function Layout() {
-  const location = useLocation();
-  const hideLayout = location.pathname === "/login";
+  const location = useLocation();
+  const hideLayout = location.pathname === "/login";
 
-  return (
-    <div className="flex min-h-screen bg-gray-50">
-      {!hideLayout && <Sidebar />}
-      <div className="flex flex-col flex-1">
-        {!hideLayout && <Navbar />}
-        <main className="flex-1 p-6 overflow-y-auto">
-          <Outlet />
-        </main>
-      </div>
-    </div>
-  );
+  return (
+    <div className="flex min-h-screen bg-gray-50">
+      {!hideLayout && <Sidebar />}
+      <div className="flex flex-col flex-1">
+        {/* !hideLayout && <Navbar /> // Removido */}
+        <main className="flex-1 p-6 overflow-y-auto">
+          <Outlet />
+        </main>
+      </div>
+    </div>
+  );
 }
