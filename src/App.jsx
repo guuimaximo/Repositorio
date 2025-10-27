a7ee47cb3c1d092dee93ba3927c6b65c5f17f4f6
guuimaximo <guimaximo1321@gmail.com>
2025-10-26 22:40:08 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+// src/App.jsx
+
 import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./components/Layout";
 
@@ -8,31 +10,31 @@ import TratarTratativa from "./pages/TratarTratativa";
 import ConsultarTratativa from "./pages/ConsultarTratativa";
 import SolicitacaoTratativa from "./pages/SolicitacaoTratativa";
 import Login from "./pages/Login";
+import Cadastro from "./pages/Cadastro"; // 1. Importar Cadastro
 import LancarAvaria from "./pages/LancarAvaria"; 
 import CobrancasAvarias from "./pages/CobrancasAvarias";
-import AprovacaoAvarias from "./pages/AprovacaoAvarias"; // Importação Adicionada
+import AprovacaoAvarias from "./pages/AprovacaoAvarias"; 
 
 export default function App() {
   return (
     <Routes>
-      {/* Rotas internas com Sidebar + Navbar */}
+      {/* Rotas internas (protegidas) */}
       <Route element={<Layout />}>
         <Route path="/" element={<Dashboard />} />
         <Route path="/central" element={<CentralTratativas />} />
         <Route path="/tratar/:id" element={<TratarTratativa />} />
         <Route path="/consultar/:id" element={<ConsultarTratativa />} />
         <Route path="/solicitar" element={<SolicitacaoTratativa />} />
-        
-        {/* Rotas de Avarias */}
         <Route path="/lancar-avaria" element={<LancarAvaria />} />
-        <Route path="/aprovar-avarias" element={<AprovacaoAvarias />} /> {/* Rota Adicionada */}
+        <Route path="/aprovar-avarias" element={<AprovacaoAvarias />} />
         <Route path="/cobrancas" element={<CobrancasAvarias />} />
       </Route>
 
-      {/* Página de Login sem layout */}
+      {/* Rotas públicas (sem layout) */}
       <Route path="/login" element={<Login />} />
+      <Route path="/cadastro" element={<Cadastro />} /> {/* 2. Adicionar Rota */}
 
-      {/* Redirecionamento padrão */}
+      {/* Redirecionamento padrão (vamos ajustar isso no Layout) */}
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
   );
