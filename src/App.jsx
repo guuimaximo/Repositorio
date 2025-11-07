5a3f70b04a6c0bf5ffe6f144bf0763594340df4e
guuimaximo <guimaximo1321@gmail.com>
2025-11-07 15:15:01 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,34 +9,27 @@ import CentralTratativas from "./pages/CentralTratativas";
 import TratarTratativa from "./pages/TratarTratativa";
 import ConsultarTratativa from "./pages/ConsultarTratativa";
 import SolicitacaoTratativa from "./pages/SolicitacaoTratativa";
-// import Login from "./pages/Login"; // Comentado
-// import Cadastro from "./pages/Cadastro"; // Comentado
 import LancarAvaria from "./pages/LancarAvaria"; 
 import CobrancasAvarias from "./pages/CobrancasAvarias";
 import AprovacaoAvarias from "./pages/AprovacaoAvarias"; 
+import AvariasEmRevisao from "./pages/AvariasEmRevisao"; // ✅ Nova página adicionada
 
 export default function App() {
-  return (
-    <Routes>
-      {/* Rotas internas (agora públicas temporariamente) */}
-      <Route element={<Layout />}>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/central" element={<CentralTratativas />} />
-        <Route path="/tratar/:id" element={<TratarTratativa />} />
-        <Route path="/consultar/:id" element={<ConsultarTratativa />} />
-        <Route path="/solicitar" element={<SolicitacaoTratativa />} />
-        <Route path="/lancar-avaria" element={<LancarAvaria />} />
-        <Route path="/aprovar-avarias" element={<AprovacaoAvarias />} />
-        <Route path="/cobrancas" element={<CobrancasAvarias />} />
-        <Route path="/avarias-reprovadas" element={<AvariasReprovadas />} />
-      </Route>
+  return (
+    <Routes>
+      <Route element={<Layout />}>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/central" element={<CentralTratativas />} />
+        <Route path="/tratar/:id" element={<TratarTratativa />} />
+        <Route path="/consultar/:id" element={<ConsultarTratativa />} />
+        <Route path="/solicitar" element={<SolicitacaoTratativa />} />
+        <Route path="/lancar-avaria" element={<LancarAvaria />} />
+        <Route path="/aprovar-avarias" element={<AprovacaoAvarias />} />
+        <Route path="/cobrancas" element={<CobrancasAvarias />} />
+        <Route path="/avarias-em-revisao" element={<AvariasEmRevisao />} /> {/* ✅ Rota atualizada */}
+      </Route>
 
-      {/* Rotas públicas comentadas */}
-      {/* <Route path="/login" element={<Login />} /> */}
-      {/* <Route path="/cadastro" element={<Cadastro />} /> */}
-
-      {/* Redirecionamento padrão */}
-      <Route path="*" element={<Navigate to="/" replace />} /> 
-    </Routes>
-  );
+      <Route path="*" element={<Navigate to="/" replace />} /> 
+    </Routes>
+  );
 }
