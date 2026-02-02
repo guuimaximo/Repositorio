183dd4ddf948244fddfc99c10b3bc299cc7ce1ef
Guilherme Maximo <guimaximo1321@gmail.com>
2026-02-02 14:35:16 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import TratativasResumo from "./pages/TratativasResumo";
 import TratarTratativa from "./pages/TratarTratativa";
 import ConsultarTratativa from "./pages/ConsultarTratativa";
 import SolicitacaoTratativa from "./pages/SolicitacaoTratativa";
+import TratativasRH from "./pages/TratativasRH"; // ✅ NOVO
 
 import LancarAvaria from "./pages/LancarAvaria";
 import CobrancasAvarias from "./pages/CobrancasAvarias";
@@ -94,7 +95,7 @@ export default function App() {
 
           {/* ✅ PCM */}
           <Route path="/pcm-inicio" element={<PCMInicio />} />
-          <Route path="/pcm-resumo" element={<PCMResumo />} /> {/* ✅ NOVO */}
+          <Route path="/pcm-resumo" element={<PCMResumo />} />
           <Route path="/pcm-diario/:id" element={<PCMDiario />} />
 
           {/* Tratativas */}
@@ -103,6 +104,9 @@ export default function App() {
           <Route path="/tratar/:id" element={<TratarTratativa />} />
           <Route path="/consultar/:id" element={<ConsultarTratativa />} />
           <Route path="/solicitar" element={<SolicitacaoTratativa />} />
+
+          {/* ✅ RH - Tratativas (NOVO) */}
+          <Route path="/tratativas-rh" element={<TratativasRH />} />
 
           {/* Avarias */}
           <Route path="/lancar-avaria" element={<LancarAvaria />} />
